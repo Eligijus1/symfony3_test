1d9af5e1e5ff67ea294aebe8c36c09a66a923bb0
Eligijus Stugys <eligijus.stugys@gmail.com>
2016-04-03 04:59:14 -0400

    Implementing user managing.

---
+++ src/Test1Bundle/Form/UserType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Test1Bundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('password')
+            ->add('email')
+            ->add('isActive')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Test1Bundle\Entity\User'
+        ));
+    }
+}
