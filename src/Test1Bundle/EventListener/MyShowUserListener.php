470cd13ea0f96d898ba29c1ed8245255210ffbf5
Eligijus Stugys <eligijus.stugys@nfq.lt>
2016-04-08 15:53:58 +0300

    Implemented User Navbar (right) and Sidebar (left).

---
+++ src/Test1Bundle/EventListener/MyShowUserListener.php
@@ -0,0 +1,42 @@
+<?php
+namespace Test1Bundle\EventListener;
+
+use Avanzu\AdminThemeBundle\Event\ShowUserEvent;
+use Test1Bundle\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+
+class MyShowUserListener
+{
+    /**
+     * @var TokenStorageInterface
+     */
+    private $tokenStorage;
+
+    /**
+     * @param TokenStorageInterface $token_storage
+     */
+    public function __construct(TokenStorageInterface $token_storage)
+    {
+        $this->tokenStorage = $token_storage;
+    }
+
+    /**
+     * @param ShowUserEvent $event
+     */
+    public function onShowUser(ShowUserEvent $event) {
+
+        $user = $this->getUser();
+        $event->setUser($user);
+
+    }
+
+    /**
+     * Return logged in user object.
+     *
+     * @return User
+     */
+    protected function getUser() {
+        return $this->tokenStorage->getToken()->getUser();
+    }
+}
