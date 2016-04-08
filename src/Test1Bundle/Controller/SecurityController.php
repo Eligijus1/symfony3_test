48e1b416b6b398c910739f58bfb7f523c15302eb
Eligijus Stugys <eligijus.stugys@nfq.lt>
2016-04-08 14:37:48 +0300

    Fixing security system.

--- src/Test1Bundle/Controller/SecurityController.php
+++ src/Test1Bundle/Controller/SecurityController.php
@@ -5,29 +5,51 @@ namespace Test1Bundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class SecurityController extends Controller
 {
     /**
-     * @Route("/login", name="login")
+     * @Route("/login", name="security_login_form")
+     * @Method("GET")
      */
-    public function loginAction(Request $request)
+    public function loginAction()
     {
         $authenticationUtils = $this->get('security.authentication_utils');
-
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render(
             'Test1Bundle:Security:login.html.twig',
             array(
                 // last username entered by the user
-                'last_username' => $lastUsername,
-                'error'         => $error,
+                'last_username' => $authenticationUtils->getLastUsername(),
+                'error'         => $authenticationUtils->getLastAuthenticationError(),
             )
         );
+    }
+
+    /**
+     * This is the route the login form submits to.
+     *
+     * But, this will never be executed. Symfony will intercept this first
+     * and handle the login automatically. See form_login in app/config/security.yml
+     *
+     * @Route("/login_check", name="security_login_check")
+     */
+    public function loginCheckAction()
+    {
+        throw new \Exception('Login check action should never be reached!');
+    }
+
+    /**
+     * This is the route the user can use to logout.
+     *
+     * But, this will never be executed. Symfony will intercept this first
+     * and handle the logout automatically. See logout in app/config/security.yml
+     *
+     * @Route("/logout", name="security_logout")
+     */
+    public function logoutAction()
+    {
+        throw new \Exception('Logout check action should never be reached!');
     }
 }
