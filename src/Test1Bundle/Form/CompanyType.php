dc9be1c6d023b3ca82a34a91964e9abe4c223272
Eligijus Stugys <eligijus.stugys@nfq.lt>
2016-08-24 00:47:09 +0300

    Companies interface.

---
+++ src/Test1Bundle/Form/CompanyType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Test1Bundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CompanyType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('createBy')
+            ->add('createDate', DateTimeType::class)
+            ->add('modifyBy')
+            ->add('modifyDate', DateTimeType::class)
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Test1Bundle\Entity\Company'
+        ));
+    }
+}
