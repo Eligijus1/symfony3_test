b48d4d3f6fe093cc2352cdb49c8216e74a431a97
Eligijus Stugys <eligijus.stugys@nfq.lt>
2016-09-01 18:46:28 +0300

    Implemented groups.

---
+++ src/Test1Bundle/Entity/Group.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @copyright C UAB NFQ Technologies 2016
+ *
+ * This Software is the property of NFQ Technologies
+ * and is protected by copyright law – it is NOT Freeware.
+ *
+ * Any unauthorized use of this software without a valid license key
+ * is a violation of the license agreement and will be prosecuted by
+ * civil and criminal law.
+ *
+ * Contact UAB NFQ Technologies:
+ * E-mail: info@nfq.lt
+ * http://www.nfq.lt
+ */
+
+namespace Test1Bundle\Entity;
+
+use FOS\UserBundle\Model\Group as BaseGroup;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_group")
+ */
+class Group extends BaseGroup
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+}
--- src/Test1Bundle/Entity/User.php
+++ src/Test1Bundle/Entity/User.php
@@ -18,9 +18,136 @@ class User extends BaseUser
      */
     protected $id;
 
+    /**
+     * @ORM\ManyToMany(targetEntity="Test1Bundle\Entity\Group")
+     * @ORM\JoinTable(name="fos_user_user_group",
+     *      joinColumns={@ORM\JoinColumn(name="user_id", referencedColumnName="id")},
+     *      inverseJoinColumns={@ORM\JoinColumn(name="group_id", referencedColumnName="id")}
+     * )
+     */
+    protected $groups;
+
     public function __construct()
     {
         parent::__construct();
         // your own logic
+    }
+
+    public function getUsername()
+    {
+        return $this->username;
+    }
+    public function getSalt()
+    {
+        // you *may* need a real salt depending on your encoder
+        // see section on salt below
+        return null;
+    }
+    public function getPassword()
+    {
+        return $this->password;
+    }
+    public function getRoles()
+    {
+        return array('ROLE_USER', 'ROLE_ADMIN');
+    }
+    public function eraseCredentials()
+    {
+    }
+    /** @see \Serializable::serialize() */
+    public function serialize()
+    {
+        return serialize(array(
+            $this->id,
+            $this->username,
+            $this->password,
+            // see section on salt below
+            // $this->salt,
+        ));
+    }
+    /** @see \Serializable::unserialize() */
+    public function unserialize($serialized)
+    {
+        list (
+            $this->id,
+            $this->username,
+            $this->password,
+            // see section on salt below
+            // $this->salt
+            ) = unserialize($serialized);
+    }
+    /**
+     * @param string $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+    /**
+     * @param string $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+    /**
+     * @param string $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+    /**
+     * @param bool $isActive
+     */
+    public function setIsActive($isActive)
+    {
+        $this->isActive = $isActive;
+    }
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function getAvatar()
+    {
+        // TODO: Implement getAvatar() method.
+    }
+    public function getName()
+    {
+        return $this->username;
+    }
+    public function getMemberSince()
+    {
+        // TODO: Implement getMemberSince() method.
+    }
+    public function isOnline()
+    {
+        return true;
+    }
+    public function getIdentifier()
+    {
+        return $this->id;
+    }
+    public function getTitle()
+    {
+        return $this->username . " " . $this->email;
     }
 }
