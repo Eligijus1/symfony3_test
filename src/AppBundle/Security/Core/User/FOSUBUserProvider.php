52c32f7eb2da3697cc1296a19146a8597be09d8e
Eligijus Stugys <eligijus.stugys@nfq.lt>
2017-09-18 14:16:10 +0300

    Latest fixes.

--- src/AppBundle/Security/Core/User/FOSUBUserProvider.php
+++ src/AppBundle/Security/Core/User/FOSUBUserProvider.php
@@ -15,18 +15,21 @@ class FOSUBUserProvider extends BaseClass
     {
         $property = $this->getProperty($response);
         $username = $response->getUsername();
-        //on connect - get the access token and the user ID
+
+        //On connect - get the access token and the user ID
         $service = $response->getResourceOwner()->getName();
         $setter = 'set' . ucfirst($service);
         $setter_id = $setter . 'Id';
         $setter_token = $setter . 'AccessToken';
-        //we "disconnect" previously connected users
+
+        //We "disconnect" previously connected users:
         if (null !== $previousUser = $this->userManager->findUserBy(array($property => $username))) {
             $previousUser->$setter_id(null);
             $previousUser->$setter_token(null);
             $this->userManager->updateUser($previousUser);
         }
-        //we connect current user
+
+        //We connect current user:
         $user->$setter_id($username);
         $user->$setter_token($response->getAccessToken());
         $this->userManager->updateUser($user);
