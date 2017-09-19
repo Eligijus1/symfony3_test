cb023dbf10507974b6b84289c85dd3398c826fab
Eligijus Stugys <eligijus.stugys@nfq.lt>
2017-09-19 10:16:39 +0300

    Updated tools.

--- tools/reload_cache.sh
+++ tools/reload_cache.sh
@@ -1,6 +1,15 @@
 #!/bin/bash          
 # cd ../
-sudo chown -R www-data:www-data var
-sudo chmod -R 777 var
+
+sudo chown -R www-data:vagrant /home/vagrant/symfony3_test
+sudo chmod -R 775 /home/vagrant/symfony3_test
+
+rm -fr /home/vagrant/symfony3_test/var
+php /home/vagrant/symfony3_test/bin/console cache:clear --env=prod
+php /home/vagrant/symfony3_test/bin/console cache:clear --env=dev
+php /home/vagrant/symfony3_test/bin/console cache:clear --env=test
+
+sudo chown -R www-data:vagrant /home/vagrant/symfony3_test
+sudo chmod -R 775 /home/vagrant/symfony3_test
 
 
